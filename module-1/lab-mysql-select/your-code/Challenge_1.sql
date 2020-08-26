9a3d4f90b0e67be2fd429ef643737f64d8059fac
Javi Martínez <javier.martifer@gmail.com>
2020-08-26 20:10:28 +0200

    lab completo sin bonus

---
+++ module-1/lab-mysql-select/your-code/Challenge_1.sql
@@ -0,0 +1,9 @@
+SELECT 
+au.au_id AS AUTHOR_ID, au.au_lname AS LAST_NAME, au.au_fname AS FIRST_NAME, 
+ti.title AS TITLE,
+pu.pub_name AS PUBLISHERS
+FROM publications.authors AS au
+LEFT JOIN publications.titleauthor AS tiau ON au.au_id = tiau.au_id
+LEFT JOIN publications.titles AS ti ON tiau.title_id = ti.title_id
+LEFT JOIN publications.publishers AS pu ON ti.pub_id = pu.pub_id
+ORDER BY AUTHOR_ID;
