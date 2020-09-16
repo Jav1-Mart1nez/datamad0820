b6ba390b3c48b824b1a17a9eaad21fad89abe545
Javi Martínez <javier.martifer@gmail.com>
2020-09-16 22:37:25 +0200

    [lab-code-simplicity-efficiency]

--- module-1/lab-code-simplicity-efficiency/your-code/challenge-2.py
+++ module-1/lab-code-simplicity-efficiency/your-code/challenge-2.py
@@ -7,32 +7,30 @@ The code is functional but has a lot of room for improvement. Use what you have 
 about simple and efficient code, refactor the code.
 """
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
-    s = ''
-    while p<l:
-        import random
-        s += random.choice(a)
-        p += 1
-    return s
+import random
+import sys
+import string
 
-def BatchStringGenerator(n, a=8, b=12):
-    r = []
-    for i in range(n):
-        c = None
-        if a < b:
-            import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
+def RandomStringGenerator(l=12):
+    
+    return "".join(random.choice(string.ascii_lowercase+string.digits) for i in range(l))
+   
+
+def BatchStringGenerator(len_list, min_str=8, max_str=12):
+    
+    random_list = []
+    for i in range(len_list):
+        if min_str < max_str:
+            len_strs = random.choice(range(min_str, max_str))
+        elif min_str == max_str:
+            len_strs = min_str
         else:
-            import sys
             sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
-    return r
+        random_list.append(RandomStringGenerator(len_list))
+    return random_list
 
-a = input('Enter minimum string length: ')
-b = input('Enter maximum string length: ')
-n = input('How many random strings to generate? ')
+min_str = input('Enter minimum string length: ')
+max_str = input('Enter maximum string length: ')
+len_list = input('How many random strings to generate? ')
 
-print(BatchStringGenerator(int(n), int(a), int(b)))
+print(BatchStringGenerator(int(len_list), int(min_str), int(max_str)))
